757d698cf73416e008fcc99369640be8343a7153
Martin Herold <martin3421@gmail.com>
2019-09-24 10:18:06 +0200

    rest of project

--- leadmanager/leads/models.py
+++ leadmanager/leads/models.py
@@ -1,8 +1,4 @@
 from django.db import models
-
-# Create your models here.
-
-from django.db import models
 from django.contrib.auth.models import User
 
 
@@ -10,4 +6,6 @@ class Lead(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100, unique=True)
     message = models.CharField(max_length=500, blank=True)
+    owner = models.ForeignKey(
+        User, related_name="leads", on_delete=models.CASCADE, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
